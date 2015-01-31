c7701ffeec3b851e8d5c51aa1b469527aba2b56a
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-01-31 14:34:15 +0700

    Iniiial commit

---
+++ lib/App/eta2zero.pm
@@ -0,0 +1,15 @@
+package App::eta2zero;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Display countdown progress bar
+
+=for Pod::Coverage ^(.+)$
+
+=head1 SYNOPSIS
+
+See the command-line scripts L<eta2zero>.
+
+=cut
